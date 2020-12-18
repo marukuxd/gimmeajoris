e0a1021ca36e3bffd5f195fb944ff494532ebae6
marukuxd <contact@maruku.be>
2020-12-18 23:07:25 +0100

    yo

---
+++ main.js
@@ -0,0 +1,17 @@
+const pre = ["jo", "bo", "ro", "zo", "to",];
+var suf = ["tis", "bis", "jis", "dis", "tis", "techtv"];
+const lol = document.getElementById("anal");
+
+
+
+function joris() {
+    var a = Math.floor(Math.random() * Math.floor(pre.length));
+    var b = Math.floor(Math.random() * Math.floor(suf.length));
+    lol.innerHTML = pre[a] + suf[b];
+    return pre[a] + suf[b];
+    
+}
+
+
+
+joris();
